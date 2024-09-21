bfb61f290013a0cbc9de687b7eb484cd8756bc2a
CaioAraujoP <caio2007araujopereira@gmail.com>
2024-09-21 09:07:50 -0300

    Delete Aula 03- Manipulação de String directory

--- Aula 03- Manipulação de String/joinSeparate.py
+++
@@ -1,9 +0,0 @@
-texto = 'Alex Caio Ewerton Isaac Ruth Sthefane Matheus'
-
-#Juntar todos os caracteres e os separar um delimitador
-joinString = '-'.join(texto)
-print(joinString)
-
-#Divide as palavras de um texto
-sliptString = texto.split()
-print(sliptString[-1])
--- Aula 03- Manipulação de String/teste.py
+++
@@ -1,3 +0,0 @@
-cidade = str(input("Digite o nome de uma cidade: "))
-cidade1 = cidade.split()[0]
-print("Santos" in cidade1)  
